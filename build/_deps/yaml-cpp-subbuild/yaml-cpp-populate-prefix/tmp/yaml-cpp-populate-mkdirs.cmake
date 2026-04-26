e2a73035aef7221db44ac56d1925a704bd010ac8
HeliosZhou <shengyang.zhou.0615@gmail.com>
2026-04-26 19:07:26 +0800

    cmake

---
+++ build/_deps/yaml-cpp-subbuild/yaml-cpp-populate-prefix/tmp/yaml-cpp-populate-mkdirs.cmake
@@ -0,0 +1,22 @@
+# Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+# file Copyright.txt or https://cmake.org/licensing for details.
+
+cmake_minimum_required(VERSION 3.5)
+
+file(MAKE_DIRECTORY
+  "/home/zsy/LLMSimulator/src/dram/ramulator2/ext/yaml-cpp"
+  "/home/zsy/LLMSimulator/build/_deps/yaml-cpp-build"
+  "/home/zsy/LLMSimulator/build/_deps/yaml-cpp-subbuild/yaml-cpp-populate-prefix"
+  "/home/zsy/LLMSimulator/build/_deps/yaml-cpp-subbuild/yaml-cpp-populate-prefix/tmp"
+  "/home/zsy/LLMSimulator/build/_deps/yaml-cpp-subbuild/yaml-cpp-populate-prefix/src/yaml-cpp-populate-stamp"
+  "/home/zsy/LLMSimulator/build/_deps/yaml-cpp-subbuild/yaml-cpp-populate-prefix/src"
+  "/home/zsy/LLMSimulator/build/_deps/yaml-cpp-subbuild/yaml-cpp-populate-prefix/src/yaml-cpp-populate-stamp"
+)
+
+set(configSubDirs )
+foreach(subDir IN LISTS configSubDirs)
+    file(MAKE_DIRECTORY "/home/zsy/LLMSimulator/build/_deps/yaml-cpp-subbuild/yaml-cpp-populate-prefix/src/yaml-cpp-populate-stamp/${subDir}")
+endforeach()
+if(cfgdir)
+  file(MAKE_DIRECTORY "/home/zsy/LLMSimulator/build/_deps/yaml-cpp-subbuild/yaml-cpp-populate-prefix/src/yaml-cpp-populate-stamp${cfgdir}") # cfgdir has leading slash
+endif()
---
+++ build/src/dram/CMakeFiles/dram.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for dram.
+# This may be replaced when dependencies are built.
