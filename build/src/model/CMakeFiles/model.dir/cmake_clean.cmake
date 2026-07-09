c9a3bb12417a448245f7df7194127fdbe8b6ad4e
HeliosZhou <shengyang.zhou.0615@gmail.com>
2026-07-09 11:25:34 +0800

    chore(repo): stop tracking build artifacts

--- build/src/model/CMakeFiles/model.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/model.dir/llm.cpp.o"
-  "CMakeFiles/model.dir/llm.cpp.o.d"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/model.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
